9d1891dbed19bb7a734ab71a518516cb8405fd37
OdilKhasanov <okhasanov1995@gmail.com>
2019-06-12 14:23:04 -0700

    Hello git!

--- src/Hello/HelloWorld.java
+++ src/Hello/HelloWorld.java
@@ -3,9 +3,8 @@ package Hello;
 public class HelloWorld {
 	public static void main(String[] args) {
 
-		System.out.println("Hello World");
-		System.out.println("Hello World");
-		System.out.println("Hello World");
+		System.out.println("Hello Github");
+		
 		
 	}
 }
